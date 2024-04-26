e75dc0f7ebd8d98cff41c126af8864a102e6cac3
lgswin <lgswin@gmail.com>
2024-04-26 07:27:08 -0400

    message list

--- firebase-chat/app/(app)/home.js
+++ firebase-chat/app/(app)/home.js
@@ -1,19 +1,50 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useAuth } from '../../context/authContext'
+import { StatusBar } from 'expo-status-bar';
+import ChatList from '../../components/ChatList';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { doc, getDocs, query, where } from 'firebase/firestore';
+import { usersRef } from '../../firebaseConfig';
 
 export default function Home() {
   const {logout, user} = useAuth();
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=> {
+    if(user?.uid)
+      getUsers();  
+  },[])
+  const getUsers = async()=>{
+    // fetch users
+    const q = query(usersRef, where('userId', '!=', user?.uid));
+
+    const querySnapshot = await getDocs(q);
+    let data=[];
+    querySnapshot.forEach(doc=>{
+      data.push({...doc.data()});
+    });
+
+    setUsers(data);
+  }
+
   const handleLogout = async ()=> {
     await logout();
   }
   console.log('user data: ', user);
   return (
     <View className="flex-1 bg-white">
-      <Text>Home....</Text>
-      <Pressable onPress={handleLogout}>
-        <Text>Sign out</Text>
-      </Pressable>
+      <StatusBar style="light" />
+      {
+        users.length>0? (
+          <ChatList currentUser={user} users={users} />
+        ):(
+          <View className="flex item-center" style={{top: hp(30)}}>
+            <ActivityIndicator size="large" />
+          </View>
+        )
+      }
+
     </View>
   )
 }
--- firebase-chat/components/CustomKeyboardView.js
+++ firebase-chat/components/CustomKeyboardView.js
@@ -3,15 +3,25 @@ import React from 'react'
 
 const ios = Platform.Os == 'ios';
 
-export default function CustomKeyboardView({children}) {
+export default function CustomKeyboardView({children, inChat}) {
+  let keyConfig = {};
+  let scrollViewConfig = {};
+
+  if (inChat) {
+    keyConfig = {keyboardVerticalOffset: 90};
+    scrollViewConfig = {contentContainerStyle: {flex: 1}};
+  }
+
   return (
     <KeyboardAvoidingView
         behavior={ios? 'padding': 'height'}
-        style={{flex: 1}}>
+        style={{flex: 1}}
+        {...keyConfig}>
             <ScrollView
                 style={{flex: 1}}
                 bounces={false}
                 showsVerticalScrollIndicator={false}
+                {...scrollViewConfig}
                 >
                     {children}
             </ScrollView>
